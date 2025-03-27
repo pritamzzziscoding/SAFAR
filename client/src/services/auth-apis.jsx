578c9bea7a9b7ab6f05a12b91cda6c24ddaeb406
Pritam Barman <pritambarmanofficial1608@gmail.com>
2025-03-27 16:34:02 +0530

    logout

--- client/src/services/auth-apis.jsx
+++ client/src/services/auth-apis.jsx
@@ -1,10 +1,15 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "/auth"
+    baseURL: "http://localhost:8080"
 })
 
 export const login = (body) => {
     const res = api.post("/login", body)
     return res;
+}
+
+export const logout = () => {
+    const res = api.get("/logout")
+    return res
 }
